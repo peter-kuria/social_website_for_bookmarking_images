b4890376088727ebe54efd97e0033a7ccf6e170c
Peter Kuria <petermbaik@gmail.com>
2021-05-09 12:12:01 +0300

    added regisstration form

--- account/urls.py
+++ account/urls.py
@@ -22,4 +22,5 @@ path('', views.dashboard, name='dashboard'),
     #auth_views.PasswordResetCompleteView.as_view(),
     #name='password_reset_complete'),
 path('', include('django.contrib.auth.urls')),
+path('register/', views.register, name='register'),
 ]
